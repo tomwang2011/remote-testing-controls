829de03c647d6cb4784659811f0d020269acefee
tomwang2011 <tong.wang@liferay.com>
2016-04-29 12:02:36 -0700

    Add logic for portal-impl tests

--- src/remote/tasks/TestTask.java
+++ src/remote/tasks/TestTask.java
@@ -14,13 +14,18 @@
 
 package remote.tasks;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -45,6 +50,14 @@ public class TestTask extends Task {
 			Path fullPath = _findTest(properties);
 
 			if (fullPath != null) {
+				Path portalDir = Paths.get(
+					properties.getProperty("portal.dir"));
+
+				Path portalImplPath = portalDir.resolve("portal-impl");
+
+				if (fullPath.startsWith(portalImplPath)) {
+					_runPortalImplTests(portalImplPath, properties);
+				}
 
 			}
 			else {
@@ -107,6 +120,69 @@ public class TestTask extends Task {
 		}
 
 		return _path;
+	}
+
+	private void _runPortalImplTests(Path portalImplPath, Properties properties)
+			throws IOException {
+
+		Path portalImplTestPath = Paths.get(
+			properties.getProperty("portal.dir"), "portal-impl/test");
+
+		Path backupFilePath = portalImplTestPath.resolve(
+			"portal-test-ext.properties.backup");
+
+		Path portalTestExtPath = portalImplTestPath.resolve(
+			"portal-test-ext.properties");
+
+		if (Files.exists(portalTestExtPath)) {
+			Files.copy(
+				portalTestExtPath, backupFilePath,
+				StandardCopyOption.REPLACE_EXISTING);
+		}
+
+		TaskUtils.createTestExtProperties(portalTestExtPath, _dbType);
+
+		List<String> testTask = new ArrayList<>();
+
+		testTask.add("ant");
+		testTask.add("test-class");
+		testTask.add("-Dtest.class=" + _testClass);
+
+		boolean debug = Boolean.valueOf(properties.getProperty("debug"));
+
+		if (debug) {
+			testTask.add("-Djvm.debug=true");
+		}
+
+		ProcessBuilder processBuilder = new ProcessBuilder(testTask);
+
+		processBuilder.directory(portalImplPath.toFile());
+
+		Process process = processBuilder.start();
+
+		String line;
+
+		try(BufferedReader br = new BufferedReader(
+			new InputStreamReader(process.getInputStream()))) {
+
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		}
+
+		try(BufferedReader br = new BufferedReader(
+			new InputStreamReader(process.getErrorStream()))) {
+
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		}
+
+		if (Files.exists(backupFilePath)) {
+			Files.move(
+				backupFilePath, portalTestExtPath,
+				StandardCopyOption.REPLACE_EXISTING);
+		}
 	}
 
 	private void _setPath(Path path) {
